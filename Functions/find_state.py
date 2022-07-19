29a1a24c584326fe5eab2643e643b3c881796b69
Andréa <andreaepivent>
2022-07-19 11:42:56 +0200

    delete function folder

--- Functions/find_state.py
+++
@@ -1,7 +0,0 @@
-def find_rowindex(S,row,col):
-    row_index = -1
-    state_space = S.shape[0]
-    for i in range(state_space):
-            if (S[i,0] == row) & (S[i,1] == col):
-                row_index = i
-    return(row_index)
